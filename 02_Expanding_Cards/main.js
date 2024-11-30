ad7bba5f5db34a5d6c3584cb43f53aebcfde69ad
Sunanda Roy <roysunanda.update@gmail.com>
2024-12-01 00:01:30 +0530

    #1 & #2 project uploaded

---
+++ 02_Expanding_Cards/main.js
@@ -0,0 +1,16 @@
+// console.log("working");
+
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
